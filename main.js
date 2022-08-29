da10f80bc5ea5547cf6f13b63d65693ecfd41bb4
DonPastor <marijan.pastor@gmail>
2022-08-29 19:57:10 +0200

    conditional rendering

--- main.js
+++ main.js
@@ -4,7 +4,8 @@ const app = Vue.createApp ({
             product: 'Socks',
             description: 'Premium winter cotton socks',
             image: './assets/images/socks_green.jpg',
-            url: 'https://www.vuemastery.com/'
+            url: 'https://www.vuemastery.com/',
+            inStock: true
         }
     }
 })
