e5f336456d5034534a2982a017a1277f7379c1e2
DonPastor <marijan.pastor@gmail>
2022-08-29 20:26:05 +0200

    List rendering

--- main.js
+++ main.js
@@ -2,11 +2,18 @@ const app = Vue.createApp ({
     data() {
         return {
             product: 'Socks',
-            description: 'Premium winter cotton socks',
             image: './assets/images/socks_green.jpg',
-            url: 'https://www.vuemastery.com/',
-            inventory: 8,
-            onSale: true
+            inStock: true,
+            details: ['50% cotton', '30% wool', '20% polyester'],
+            variants: [
+                {id: 2234, color: 'green'},
+                {id: 2235, color: 'blue' },
+            ],
+            sizes: [
+                {id: 2236, size: 'S'},
+                {id: 2237, size: 'M'},
+                {id: 2238, size: 'L'},
+            ] 
         }
     }
 })
