98319782693d6907d01d83026fe6ffbaaf5c3b02
DonPastor <marijan.pastor@gmail>
2022-08-29 21:05:42 +0200

    Event handling

--- main.js
+++ main.js
@@ -1,19 +1,29 @@
 const app = Vue.createApp ({
     data() {
         return {
+            cart: 0,
             product: 'Socks',
             image: './assets/images/socks_green.jpg',
             inStock: true,
             details: ['50% cotton', '30% wool', '20% polyester'],
             variants: [
-                {id: 2234, color: 'green'},
-                {id: 2235, color: 'blue' },
+                {id: 2234, color: 'green', image: './assets/images/socks_green.jpg'},
+                {id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg' },
             ],
-            sizes: [
-                {id: 2236, size: 'S'},
-                {id: 2237, size: 'M'},
-                {id: 2238, size: 'L'},
-            ] 
+        
+        }
+    },
+    methods: {
+        addToCart() {
+            this.cart += 1
+        },
+        removeFromCart() {
+            if(this.cart > 0){
+                return this.cart -= 1
+            }
+        },
+        updateImage(variantImage){
+            this.image = variantImage
         }
     }
 })
