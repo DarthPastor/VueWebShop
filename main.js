64c13486c3615a35299fdaf4eec14f9705793cfe
DonPastor <marijan.pastor@gmail>
2022-08-29 17:26:55 +0200

    Atribute binding

--- main.js
+++ main.js
@@ -2,7 +2,9 @@ const app = Vue.createApp ({
     data() {
         return {
             product: 'Socks',
-            description: 'Premium winter cotton socks'
+            description: 'Premium winter cotton socks',
+            image: './assets/images/socks_green.jpg',
+            url: 'https://www.vuemastery.com/'
         }
     }
 })
